264c77db07af3cdfc46f29f93182f51445dd43c6
Roman - <roman0811@live.nl>
2025-04-15 20:54:02 +0200

    Created

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="w-screen h-screen bg-black text-white flex justify-center items-center text-6xl font-bold">
+      React + Vite + Tailwind Boilerplate
+    </div>
+  );
+}
+
+export default App;
